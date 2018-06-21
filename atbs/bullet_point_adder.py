7b6988e000369c47a38fc3a735995bd7fac83fb6
ChristopherSparling <christopher.j.sparling@gmail.com>
2018-06-20 21:09:01 -0400

    Added bullet_point_adder.py
    Page 137-8

---
+++ atbs/bullet_point_adder.py
@@ -0,0 +1,19 @@
+#! python 3
+"""
+Copy this for input:
+
+Line A
+Line B
+Line C
+Line D
+Line E
+"""
+import pyperclip
+
+lines = pyperclip.paste()
+print(lines) 
+
+lines = lines.split('\n')
+for index in  range(len(lines)):
+    lines[index] = '* ' + lines[index]
+    print(lines[index])
